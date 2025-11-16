87fdd3878a06a9450f322335e8ae6b65b5b0e62f
LEITTO <xxleittoxx@gmail.com>
2025-11-16 12:55:42 +0100

    celebrate errors added

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
 import { logger } from './middleware/logger.js';
 import router from './routes/notesRoutes.js';
+import { errors as celebrateErrors } from 'celebrate';
 
 const PORT = process.env.PORT ?? 3000;
 const app = express();
@@ -17,6 +18,7 @@ app.use(express.json());
 app.use(logger);
 app.use(router);
 app.use(notFoundHandler);
+app.use(celebrateErrors());
 app.use(errorHandler);
 
 await connectMongoDB();
